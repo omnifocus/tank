469feedd15ce78d410f7836ffeabb52606822649
love <shilei@myself.com>
2021-03-19 22:55:32 +0800

    ## 版本54 过滤器链可添加过滤器链

--- src/com/shilei/dp/filter/Filter1.java
+++ src/com/shilei/dp/filter/Filter1.java
@@ -6,15 +6,19 @@ import java.util.List;
 public class Filter1 {
 
     public static void main(String[] args) {
-        Message msg = new Message("<abc:)");
+        Message msg = new Message("<abc:)www.yovya.com996");
         HTMLFilter filter1 = new HTMLFilter();
         FaceFilter filter2 = new FaceFilter();
         FilterChain chain = new FilterChain();
         chain.addFilter(filter1).addFilter(filter2);
+        FilterChain chain2 = new FilterChain();
+        chain2.addFilter(new SensitiveFilter()).addFilter(new URLFilter());
+        chain.addFilter(chain2);
         chain.doFilter(msg);
         System.out.println(msg.msg);
     }
 }
+
 class Message {
     String msg;
 
@@ -28,29 +32,49 @@ interface Filter {
 }
 
 
-
 class HTMLFilter implements Filter {
 
     @Override
     public void doFilter(Message msg) {
-        msg.msg = msg.msg.replace("<","[");
+        msg.msg = msg.msg.replace("<", "[");
     }
 }
+
 class FaceFilter implements Filter {
 
     @Override
     public void doFilter(Message msg) {
-        msg.msg = msg.msg.replace(":)","^_^");
+        msg.msg = msg.msg.replace(":)", "^_^");
     }
 }
 
-class FilterChain {
+class SensitiveFilter implements Filter {
+
+    @Override
+    public void doFilter(Message msg) {
+        msg.msg = msg.msg.replace("996", "965");
+    }
+}
+
+class URLFilter implements Filter {
+
+    @Override
+    public void doFilter(Message msg) {
+        msg.msg = msg.msg.replace("www", "http://www");
+    }
+}
+
+
+class FilterChain implements Filter {
     List<Filter> filterlist = new ArrayList<>();
+
     FilterChain addFilter(Filter filter) {
         filterlist.add(filter);
         return this;
     }
-    void doFilter(Message msg) {
+
+    @Override
+    public void doFilter(Message msg) {
         for (Filter filter : filterlist) {
             filter.doFilter(msg);
         }
